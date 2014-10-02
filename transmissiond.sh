075871cf26057d829a8247833325aaf42686c03b
angel rodriguez-ricardo <angelrr7702@gmail.com>
2014-10-02 08:54:02 -0400

    Update transmissiond.sh

--- transmissiond.sh
+++ transmissiond.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 ### In transmissiond.sh (make sure this file is chmod +x):
 
-exec  /usr/bin/foreground.sh >>/var/log/transmissiond.log 2>&1
+exec  /usr/bin/transmissionfd >>/var/log/transmissiond.log 2>&1
