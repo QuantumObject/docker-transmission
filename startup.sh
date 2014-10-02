d9b3c2e8e6d77af243ab65c5790ed6487cf2c13c
angel rodriguez-ricardo <angelrr7702@gmail.com>
2014-10-02 08:57:08 -0400

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -4,22 +4,20 @@ set -e
 
 if [ -f /etc/configured ]; then
   echo 'already configured'
+else
+  PASSWD_T=${PASSWD_T:-}
+  USER_T=${USER_T:-}
+  adduser --disabled-password --gecos "" $USER_T
+  echo "$USER_T:$PASSWD_T"|chpasswd
+  sed -i 's:/user:/'"$USER_T"':g' /var/lib/transmission-daemon/info/settings.json
+  sed -i 's:\"password\":\"'"$PASSWD_T"'\":g' /var/lib/transmission-daemon/info/settings.json
+  sed -i 's:\"transmission\":\"'"$USER_T"'\":g' /var/lib/transmission-daemon/info/settings.json
+  mkdir -p /var/lib/transmission-daemon/info/Incomplete
+  mkdir -p home/$USER_T/dl/torrent
+  usermod -a -G debian-transmission $USER_T
+  chgrp debian-transmission /home/$USER_T/dl/torrent
+  chmod 770 /home/$USER_T/dl/torrent
+  #needed to fix problem with ubuntu ... and cron 
+  update-locale
+  date > /etc/configured
 fi
-
-
-PASSWD_T=${PASSWD_T:-}
-USER_T=${USER_T:-}
-adduser --disabled-password --gecos "" $USER_T
-echo "$USER_T:$PASSWD_T"|chpasswd
-sed -i 's:/user:/'"$USER_T"':g' /var/lib/transmission-daemon/info/settings.json
-sed -i 's:\"password\":\"'"$PASSWD_T"'\":g' /var/lib/transmission-daemon/info/settings.json
-sed -i 's:\"transmission\":\"'"$USER_T"'\":g' /var/lib/transmission-daemon/info/settings.json
-mkdir -p /var/lib/transmission-daemon/info/Incomplete
-mkdir -p home/$USER_T/dl/torrent
-usermod -a -G debian-transmission $USER_T
-chgrp debian-transmission /home/$USER_T/dl/torrent
-chmod 770 /home/$USER_T/dl/torrent
-#needed to fix problem with ubuntu ... and cron 
-update-locale
-date > /etc/configured
-
