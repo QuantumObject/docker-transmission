8a72ed3439f6a4cd0c6b7cef5b66e2e85c8c8a4f
angel rodriguez-ricardo <angelrr7702@gmail.com>
2014-10-19 04:02:45 -0400

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -1,1 +1,7 @@
 #!/bin/bash
+
+mkdir -p /var/backups/torrent
+cp -R /home/$USER_T/dl/torrent/* /var/backups/torrent/
+
+#Backup important file ... of the configuration ...
+cp  /etc/hosts  /var/backups/
