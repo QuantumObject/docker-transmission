d82557616a441f91c4642be921d12c68af73ad65
Angel Rodriguez-Ricardo <angelrr7702@gmail.com>
2015-11-02 09:47:56 -0500

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -5,6 +5,7 @@ set -e
 if [ -f /etc/configured ]; then
   echo 'already configured'
 else
+  groupadd debian-transmission
   PASSWD_T=${PASSWD_T:-}
   USER_T=${USER_T:-}
   adduser --disabled-password --gecos "" $USER_T
@@ -14,10 +15,11 @@ else
   sed -i 's:\"transmission\":\"'"$USER_T"'\":g' /var/lib/transmission-daemon/info/settings.json
   mkdir -p /var/lib/transmission-daemon/info/Incomplete
   mkdir -p home/$USER_T/dl/torrent
-  #usermod -a -G debian-transmission $USER_T
-  #chgrp debian-transmission /home/$USER_T/dl/torrent
+  usermod -a -G debian-transmission $USER_T
+  chgrp debian-transmission /home/$USER_T/dl/torrent
   chmod 770 /home/$USER_T/dl/torrent
   #needed to fix problem with ubuntu ... and cron 
   update-locale
+  /usr/local/bin/transmission-daemon &
   date > /etc/configured
 fi
