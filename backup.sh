40f6824d26d467a33f459246d8e4c7bee973a6d5
Angel Rodriguez-Ricardo <angelrr7702@gmail.com>
2020-04-12 09:20:21 -0400

    Delete backup.sh

--- backup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-mkdir -p /var/backups/torrent
-cp -R /home/$USER_T/dl/torrent/* /var/backups/torrent/
-
-#Backup important file ... of the configuration ...
-cp  /etc/hosts  /var/backups/
